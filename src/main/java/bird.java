a7c3397b06e50d60719606eb27a8bedf470984d6
icknee <yinqi.neo@gmail.com>
2025-01-30 12:40:06 +0800

    Add task class and mark function

--- src/main/java/bird.java
+++ src/main/java/bird.java
@@ -2,54 +2,87 @@ import java.util.Scanner;
 
 
 public class bird {
-    public static void reply(String message){
+    public static void reply(String message) {
         System.out.println("    " + message);
     }
 
-    public static void greet(){
+    public static void greet() {
         printLine();
         reply("Hello! I'm bird");
         reply("What can I do for you?");
         printLine();
     }
 
-    public static void printLine(){
+    public static void printLine() {
         System.out.println("    " + "____________________________________________________________");
     }
 
-    public static void bye(){
+    public static void bye() {
         printLine();
         reply("Bye. Hope to see you again soon!");
         printLine();
     }
 
 
-    public static void printTasks(String[] taskList, int taskCount){
+    public static void printTasks(Task[] taskList, int taskCount) {
         printLine();
-        for (int i = 0; i < taskCount; i++){
-            reply(Integer.toString(i + 1) + ". " + taskList[i]);
+        for (int i = 0; i < taskCount; i++) {
+            reply(i + 1 + ". " + "[" + taskList[i].getStatusIcon() + "] " + taskList[i].description);
         }
         printLine();
     }
 
-    public static void addTask(String[] taskList, String task, int taskCount){
+    public static void addTask(Task[] taskList, String task, int taskCount) {
         printLine();
         reply("added: " + task);
         printLine();
-        taskList[taskCount] = task;
+        Task newTask = new Task(task);
+        taskList[taskCount] = newTask;
+    }
+
+    public static void markTask(Task[] taskList, int taskNumber, int taskCount) {
+        if (taskNumber > taskCount) {
+            printLine();
+            reply("Invalid task number");
+            printLine();
+            return;
+        }
+        taskList[taskNumber - 1].setDone(true);
+        printLine();
+        reply("Nice! I've marked this task as done:");
+        reply("  " + "[" + taskList[taskNumber - 1].getStatusIcon() + "] " + taskList[taskNumber - 1].description);
+        printLine();
+    }
+
+    public static void unmarkTask(Task[] taskList, int taskNumber, int taskCount) {
+        if (taskNumber > taskCount) {
+            printLine();
+            reply("Invalid task number");
+            printLine();
+            return;
+        }
+        taskList[taskNumber - 1].setDone(false);
+        printLine();
+        reply("OK, I've marked this task as not done yet:");
+        reply("  " + "[" + taskList[taskNumber - 1].getStatusIcon() + "] " + taskList[taskNumber - 1].description);
+        printLine();
     }
 
     public static void main(String[] args) {
         greet();
         String line;
         Scanner in = new Scanner(System.in);
-        String[] taskList = new String[100];
+        Task[] taskList = new Task[100];
         int taskCount = 0;
 
         line = in.nextLine();
-        while (!line.equals("bye")){
+        while (!line.equals("bye")) {
             if (line.equals("list")) {
                 printTasks(taskList, taskCount);
+            } else if (line.startsWith("mark")) {
+                markTask(taskList, Integer.parseInt(line.split(" ")[1]), taskCount);
+            } else if (line.startsWith("unmark")) {
+                unmarkTask(taskList, Integer.parseInt(line.split(" ")[1]), taskCount);
             } else {
                 addTask(taskList, line, taskCount);
                 taskCount++;
