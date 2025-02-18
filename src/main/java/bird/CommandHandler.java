12e138fada5dad3695ac9e56d1ef254b61f2c93c
icknee <yinqi.neo@gmail.com>
2025-02-19 01:36:26 +0800

    Add delete function and use ArrayList<>

--- src/main/java/bird/CommandHandler.java
+++ src/main/java/bird/CommandHandler.java
@@ -3,17 +3,19 @@ package bird;
 import bird.task.Task;
 import bird.task.TaskHandler;
 
+import java.util.ArrayList;
+
 public class CommandHandler {
-    static int executeCommand(String command, Task[] taskList, int taskCount, String line) throws InvalidCommandException {
+    static int executeCommand(String command, ArrayList<Task> taskList, int taskCount, String line) throws InvalidCommandException {
         switch (command) {
             case "list":
                 ConsoleFormatter.printTaskList(taskList, taskCount);
                 break;
             case "mark":
-                TaskHandler.markTaskAsDone(taskList, line.split(" ")[1], taskCount);
+                TaskHandler.markTaskAsDone(taskList, line, taskCount);
                 break;
             case "unmark":
-                TaskHandler.markTaskAsNotDone(taskList, line.split(" ")[1], taskCount);
+                TaskHandler.markTaskAsNotDone(taskList, line, taskCount);
                 break;
             case "todo":
                 TaskHandler.addTodos(taskList, line, taskCount);
@@ -26,6 +28,10 @@ public class CommandHandler {
             case "event":
                 TaskHandler.addEvents(taskList, line, taskCount);
                 taskCount++;
+                break;
+            case "delete":
+                TaskHandler.deleteTask(taskList, line, taskCount);
+                taskCount--;
                 break;
             default:
                 throw new InvalidCommandException("Unknown command: " + line + "\n" + "\t" + "try entering 'list' to see the list of tasks!");
